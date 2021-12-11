c9cec04dc728a884c9d3d96464e419f42408cac0
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-12-11 13:53:20 +0200

    Added form

--- src/js/views/MapView.js
+++ src/js/views/MapView.js
@@ -1,20 +1,26 @@
 export class MapView {
   map;
+  form = document.querySelector('.form');
+
+  constructor() {}
 
   load(position) {
     const { latitude, longitude } = position.coords;
 
     this.map = L.map('map').setView([latitude, longitude], 13);
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
+    this.map.on('click', this.toogleForm);
   }
 
-  onClick(handler) {
-    handler();
+  onClick() {
     //on click logic
+  }
+
+  toogleForm() {
+    console.log('fg');
   }
 
   renderMarkers(workouts) {
